1998106f57a47dbafa5033cebb430b5d0d42c11d
Sakariye1 <sakariyeaden@gmail.com>
2024-10-01 19:24:19 +0300

    The base is ready

--- src/components/Traininglist.tsx
+++ src/components/Traininglist.tsx
@@ -5,8 +5,9 @@ import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 
 import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-material.css";
+import { ColDef } from "ag-grid-community";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
@@ -16,7 +17,6 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 import { Popconfirm } from "antd";
-import "antd/dist/antd.css";
 
 import { NewTraining, ExistingTraining } from "../../types";
 
@@ -28,64 +28,54 @@ const Traininglist: React.FC = () => {
   const [errorOpen, setErrorOpen] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useState<string>("");
 
-  //this is for the error snackbar
+  // this is for the error snackbar
   const Alert: any = React.forwardRef((props, ref: any) => (
     <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
   ));
 
-  //data for aggrid
-  const columns = [
-    { field: "activity", sortable: true, filter: true },
+  // data for ag-grid
+  const columns: ColDef<ExistingTraining>[] = [
+    { field: "activity", headerName: "Activity", sortable: true, filter: true },
     {
       headerName: "Date",
-
+      field: "date", // Ensure this field exists in ExistingTraining
       sortable: true,
       filter: true,
       valueGetter: (params: any) =>
         dayjs(params.data.date).format("DD.MM.YYYY (ddd) H:mm"),
     },
     {
-      field: "duration",
+      field: "duration", // Ensure this field exists in ExistingTraining
+      headerName: "Duration (min)",
       sortable: true,
       filter: true,
-      headerName: "Duration (min)",
       width: 150,
     },
+
     {
-      sortable: true,
-      filter: true,
-      headerName: "Customer",
-      valueGetter: (params: any) =>
-        params.data.customer == null
-          ? ""
-          : params.data.customer.firstname +
-            " " +
-            params.data.customer.lastname,
+      headerName: "",
+      field: "id", // Ensure this field exists in ExistingTraining
+      width: 65,
+      // cellRendererFramework: (params: any) => (
+      //   <EditTraining updateTraining={updateTraining} params={params} />
+      // ),
     },
     {
       headerName: "",
-      field: "id",
+      field: "id", // Ensure this field exists in ExistingTraining
       width: 65,
-      cellRendererFramework: (params: any) => (
-        <EditTraining updateTraining={updateTraining} params={params} />
-      ),
-    },
-    {
-      headerName: "",
-      field: "id",
-      width: 65,
-      cellRendererFramework: (params: any) => (
-        <Popconfirm
-          title="Are you sure you want to delete this training?"
-          onConfirm={() => deleteTraining(params.value)}
-          okText="Yes"
-          cancelText="No"
-        >
-          <IconButton color="error">
-            <DeleteIcon />
-          </IconButton>
-        </Popconfirm>
-      ),
+      // cellRendererFramework: (params: any) => (
+      //   <Popconfirm
+      //     title="Are you sure you want to delete this training?"
+      //     onConfirm={() => deleteTraining(params.value)}
+      //     okText="Yes"
+      //     cancelText="No"
+      //   >
+      //     <IconButton color="error">
+      //       <DeleteIcon />
+      //     </IconButton>
+      //   </Popconfirm>
+      // ),
     },
   ];
 
@@ -110,7 +100,7 @@ const Traininglist: React.FC = () => {
 
   const handleErrorClose = (
     event: React.SyntheticEvent | Event,
-    reason: SnackbarCloseReason,
+    reason: SnackbarCloseReason
   ): void => {
     if (reason !== "clickaway") {
       setErrorOpen(false);
@@ -152,7 +142,7 @@ const Traininglist: React.FC = () => {
   const updateTraining = (id: number, updatedTraining: ExistingTraining) => {
     editItem(
       "https://customerrest.herokuapp.com/api/trainings/" + id,
-      updatedTraining,
+      updatedTraining
     )
       .then((response) => {
         if (response.ok) {
@@ -174,12 +164,12 @@ const Traininglist: React.FC = () => {
         className="ag-theme-material"
         style={{ height: 593, width: "85%", maxWidth: "900px", margin: "auto" }}
       >
-        <AgGridReact
+        <AgGridReact<ExistingTraining>
           rowData={trainings}
           columnDefs={columns}
           pagination={true}
           paginationPageSize={10}
-          suppressCellSelection={true}
+       //   suppressCellSelection={true}
           onGridReady={onGridReady}
         />
       </div>
@@ -209,4 +199,5 @@ const Traininglist: React.FC = () => {
     </div>
   );
 };
+
 export default Traininglist;
